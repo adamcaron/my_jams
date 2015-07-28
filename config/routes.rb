28de5f3d8a9230495ca73f16cbfa74d250291ee1
Adam Caron <adamwcaron@gmail.com>
2015-07-28 16:18:14 -0600

    tests a user can be created and a registered user can login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :songs # get all 8 of those RESTful routes
-  resources :artists
+  resources :users, only: [:new, :create, :show]
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  root to: "welcome#index"
 end
